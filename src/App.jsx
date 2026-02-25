fd07d3f945c64994b6067f331f4bc46b853a6718
Olena Surilova <elen21surilova@gmail.com>
2026-02-25 23:12:48 +0100

    feat: add static list of friends

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,48 @@ const initialFriends = [
   },
 ];
 
-function App() {
+export default function App() {
 
   return (
-    <>
-      Eat 'n Split
-    </>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
   )
 }
 
-export default App
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map(friend => <Friend friend={friend} key={friend.id} />)}
+    </ul>
+  )
+}
+
+function Friend({ friend }) {
+  return <li key={friend.id}>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 0 && (
+      <p className="red">
+        You owe {friend.name} {Math.abs(friend.balance)}€
+      </p>
+    )}
+    {friend.balance > 0 && (
+      <p className="green">
+        {friend.name} owes you {Math.abs(friend.balance)}€
+      </p>
+    )}
+    {friend.balance === 0 && (
+      <p>
+        You and {friend.name} are even
+      </p>
+    )}
+
+    <button className="button">Select</button>
+  </li>
+}
